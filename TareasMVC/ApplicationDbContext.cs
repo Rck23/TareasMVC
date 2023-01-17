bae418d9260e6668f0cf28c33dd17c6cb63c7a68
Rck23 <ulimxtz@gmail.com>
2023-01-17 14:58:34 -0600

    Agregar archivos de proyecto.

---
+++ TareasMVC/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) 
+        {
+
+        }
+
+        // CONFIGURACION DE LA CLASE TAREAS PARA CONVERTIRLA EN UNA ENTIDAD
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
---
+++ TareasMVC/Entidades/Tarea.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Entidades
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int Orden { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        // PROPIEDAD DE NAVEGACIÓN (se configura automaticamente como llave foranea)
+        // Relación 1 a muchos
+        public List<Paso> Pasos { get; set; }
+
+        public List<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
