b26b8ffac9e81edcb06a66dc2fd500fc2dadd741
Ulises Martinez <ulimxtz@gmail.com>
2023-04-30 14:40:36 -0600

    Vista login TRADUCIDA

--- TareasMVC/Models/LoginViewModel.cs
+++ TareasMVC/Models/LoginViewModel.cs
@@ -6,11 +6,14 @@ namespace TareasMVC.Models
     {
         [Required(ErrorMessage = "Error.Requerido")]
         [EmailAddress(ErrorMessage = "Error.Email")]
+        [Display(Name = "Display.Email")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Error.Requerido")]
         [DataType(DataType.Password)]
+        [Display(Name = "Display.Contraseña")]
         public string Password { get; set; }
 
+      
         [Display(Name = "Recuérdame")]
         public bool Recuerdame { get; set; }
     }
